f5ef269659b37e285993f4987301d6e213c92274
sseon <rlatjsgur000@naver.com>
2023-11-19 21:38:14 +0900

    [ 02.Spring MVC ] @GetMapping과 @PostMapping(1) *. 스프링버전 바꾸기 > pom.xml > properties  - <org.springframework-version>5.0.7.RELEASE</org.springframework-version> << 바꾸고 프로젝트 우클릭 > 메이븐 > 업데이트 프로젝트 *. 뷰 컨트롤러 추가 > servlet.xml (get요청만 허용!!!)  - <view-controller path="register/add" view-name="registerForm" /> 추가
    [ 02.Spring MVC ] @GetMapping과 @PostMapping(2)
    1. @GetMapping, @PostMapping
    *. spring 4.3버전 이상부턴 @RequestMapping 대신에 사용가능
    
    2. 클래스에 붙이는 @RequestMapping
    *. 클래스 RequestMapping에 모듈명 나머지 메서드에는 get, post매핑만
    @RequestMapping("register")
    public class RgisterController {
    	@PostMapping("save")
    	public String save(User user, Model m) {}
    	@GetMapping("save")
    	public String save(User user, Model m) {}
    }
    
    3. @RequestMapping의 URL패턴
    - RequestMappingTest.java
    
    4. URL인코딩 - 퍼센트 인코딩
    - URL에 포함된 non-ASCII문자를 문자 코드(16진수) 문자열로 변환
       : 요청을 받는 서버가 어떤 OS, 어떤 인코딩을 쓰는지 모르기 때문에 요청 URL은 ASCII여야해서!!
    ex) 남궁성 -> URLEncoder.uncdoer() -> "%EB%82%A8%EA%...."
         "%EB%82%A8%EA%...." -> URLDecoder.decode() -> 남궁성
    *. URL인코딩, base64는 다름!!
    - URL인코딩 : 문자코드(숫자) -> 문자열, 문자코드(숫자) <- 문자열
    - base64(A-Z, a-z, 0~9를 6bit씩) : 바이너리 -> Text, 바이너리 <- Text

--- src/main/java/com/fastcampus/ch2/RgisterController.java
+++ src/main/java/com/fastcampus/ch2/RgisterController.java
@@ -1,17 +1,37 @@
 package com.fastcampus.ch2;
 
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // ctrl+shift+o 자동 import
 public class RgisterController {
-	@RequestMapping("register/add")
-	public String register() {
-		return "registerForm"; // WEB-INF/views/registerForm.jsp
-	}
+//	@RequestMapping(value="register/add", method={RequestMethod.GET, RequestMethod.POST})
+//	@RequestMapping("register/add") // 신규회원 가입 화면
+//	@GetMapping("register/add")
+//	public String register() {
+//		return "registerForm"; // WEB-INF/views/registerForm.jsp
+//	}
 	
-	@RequestMapping("register/save")
-	public String save() {
+//	@RequestMapping(value="register/save", method=RequestMethod.POST)
+	@PostMapping("register/save") // 4.3부터
+	public String save(User user, Model m) throws Exception{
+		// 1. 유효성 검사
+		if(!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+			
+			m.addAttribute("msg",msg);
+			return "redirect:/register/add";
+//			return "redirect:/register/add?msg="+msg; // URL재작성(rewriting)
+		}
+		
 		return "registerInfo"; // WEB-INF/views/registerInfo.jsp
+		
+	}
+
+	private boolean isValid(User user) {
+		return true;
 	}
 }
