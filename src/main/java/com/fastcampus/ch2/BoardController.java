04c9abaefe57f8c90e9c00bd91a0c520a67e3daf
sseon <rlatjsgur000@naver.com>
2024-02-06 23:08:44 +0900

    [ 02.Spring MVC ] 세션(Session) - 실습(2)
    1. 게시판 이용시, 미로그인이면 로그인 화면으로 이동
     1-1. from : request.getHeader("refer");
     1-2. to : request.getReqestURL();
    
    2. 로그인 후, 게시판으로 이동
     2-1. 보드컨트롤러에서 미로그인시 로그인폼으로 넘겨줄때 request.getRequsetURL()
           정보를 로그인폼에 넘겨주고, 이 정보를 로그인 컨트롤러에 전달해서 리다이렉트시 사용
    
    3. 세션을 시작할까 ? session = "true" or session = "false"
     3-1. 세션은 로그인 하고 나서부터 필요함
     3-2. session = "false" : 세션이 필요없는 JSP화면에 설정 + 기존 세션에 영향을 주지 않음

--- src/main/java/com/fastcampus/ch2/BoardController.java
+++ src/main/java/com/fastcampus/ch2/BoardController.java
@@ -13,7 +13,8 @@ public class BoardController {
 	@GetMapping("/list")
 	public String list(HttpServletRequest request) {
 		if(!loginCheck(request)) {
-			return "redirect:/login/login";  // 로그인을 안했으면 로그인 화면으로 이동
+			// 1. 로그인 화면에 get방식으로 이동할 url 전달하기
+			return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
 		}
 		
 		return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
--- src/main/java/com/fastcampus/ch2/LoginController.java
+++ src/main/java/com/fastcampus/ch2/LoginController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +30,8 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public String login(String id, String pwd, String toURL, boolean rememberId, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// @CookieValue("id") String cookieId >> @쿠키에 저장된 값을 받아올 수 있는 어노테이션
 		// 1. id와 pwd를 확인
 		if(!loginCheck(id, pwd)) {
 			String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
@@ -52,8 +54,10 @@ public class LoginController {
 			cookie.setMaxAge(0); // 쿠키 삭제
 			response.addCookie(cookie); 
 		}
-		// 홈으로 이동
-		return "redirect:/";
+		// 3. toURL값 체크해서 없으면 홈으로 있으면 toURL로 삼항연산 분기
+		toURL = toURL == null || toURL.equals("") ? "/" : toURL;
+		
+		return "redirect:"+toURL;
 	}
 
 	private boolean loginCheck(String id, String pwd) {
